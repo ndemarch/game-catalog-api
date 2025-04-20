933f3494c961d578964498a2392294ce9b2f4f74
Nick De Marchi <nickdemarchi@Nicks-MacBook-Air.local>
2025-04-20 19:50:58 -0400

    fixing config names and env file names

--- app/config.py
+++ app/config.py
@@ -8,17 +8,17 @@ ENV = os.getenv("ENV", "development")
 class Settings:
     def __init__(self):
         if ENV == "test":
-            self.MYSQL_USER = os.getenv("TEST_MYSQL_USER", "root")
-            self.MYSQL_PASSWORD = os.getenv("TEST_MYSQL_PASSWORD", "password")
-            self.MYSQL_DATABASE = os.getenv("TEST_MYSQL_DATABASE", "test_game_catalog")
-            self.MYSQL_HOST = os.getenv("TEST_MYSQL_HOST", "test-db")
+            self.MYSQL_USER = os.getenv("MYSQL_USER", "root")
+            self.MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD", "password")
+            self.MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "test_game_catalog")
+            self.MYSQL_HOST = os.getenv("MYSQL_HOST", "test-db")
         else:
             self.MYSQL_USER = os.getenv("MYSQL_USER", "root")
-            self.MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "password")
+            self.MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD", "password")
             self.MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "game_catalog")
             self.MYSQL_HOST = os.getenv("MYSQL_HOST", "db")
 
-        self.DATABASE_URL = f"mysql+aiomysql://{self.MYSQL_USER}:{self.MYSQL_PASSWORD}@{self.MYSQL_HOST}/{self.MYSQL_DATABASE}"
+        self.DATABASE_URL = f"mysql+aiomysql://{self.MYSQL_USER}:{self.MYSQL_ROOT_PASSWORD}@{self.MYSQL_HOST}/{self.MYSQL_DATABASE}"
         self.ENV = ENV
 
 settings = Settings()
--- tests/config.py
+++ tests/config.py
@@ -7,12 +7,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 # loading the test environment variables
 load_dotenv(".env.test")
 # testing database credentials
-MYSQL_USER = os.getenv("TEST_MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("TEST_MYSQL_PASSWORD", "password")
-MYSQL_DATABASE = os.getenv("TEST_MYSQL_DATABASE", "test_game_catalog")
-MYSQL_HOST = os.getenv("TEST_MYSQL_HOST", "test-db")
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD", "password")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "test_game_catalog")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "test-db")
 
-TEST_DATABASE_URL = f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
+TEST_DATABASE_URL = f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_ROOT_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
 # create async engine for test db
 async_engine = create_async_engine(TEST_DATABASE_URL, echo=True)
 TestingSessionLocal = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
